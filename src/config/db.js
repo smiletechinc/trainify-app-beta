fd7df704f01d424f3ea9f1b2b1a0c70bbb727b4f
Farhan-96 <farhan96fareed@gmail.com>
2022-04-22 14:42:16 +0500

    Removed calibration from beta.

--- src/config/db.js
+++ src/config/db.js
@@ -31,14 +31,25 @@ import {  Firebase, FirebaseApp, initializeApp, getApps } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+// //trainify-app-firebase #old
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDUh2w3W75cUQG2a27NxEWdNxqAZ9JzA_E",
+//   authDomain: "trainify-app-firebase.firebaseapp.com",
+//   databaseURL: "https://trainify-app-firebase-default-rtdb.firebaseio.com",
+//   projectId: "trainify-app-firebase",
+//   storageBucket: "trainify-app-firebase.appspot.com",
+//   messagingSenderId: "29770888713",
+//   appId: "1:29770888713:web:fe462fc9b13fe837ab6ad8"
+// };
+
+// trainify-app-beta #new
 const firebaseConfig = {
-  apiKey: "AIzaSyDUh2w3W75cUQG2a27NxEWdNxqAZ9JzA_E",
-  authDomain: "trainify-app-firebase.firebaseapp.com",
-  databaseURL: "https://trainify-app-firebase-default-rtdb.firebaseio.com",
-  projectId: "trainify-app-firebase",
-  storageBucket: "trainify-app-firebase.appspot.com",
-  messagingSenderId: "29770888713",
-  appId: "1:29770888713:web:fe462fc9b13fe837ab6ad8"
+  apiKey: "AIzaSyBsxLhPwhe454gtrFmWHa0q0_e1JoczbD0",
+  authDomain: "trainify-app-beta.firebaseapp.com",
+  projectId: "trainify-app-beta",
+  storageBucket: "trainify-app-beta.appspot.com",
+  messagingSenderId: "182345897118",
+  appId: "1:182345897118:web:92814fcf95bde73dc1178f"
 };
 
 // Initialize Firebase
